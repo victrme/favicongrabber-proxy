460f207cba0579dd1544014aeb9f7a366f3d1152
Victor Azevedo <34415964+victrme@users.noreply.github.com>
2022-04-29 11:07:33 +0200

    Add files via upload

--- functions/favicon.js
+++ functions/favicon.js
@@ -3,14 +3,14 @@ const fs = require('fs')
 
 exports.handler = async (event, context) => {
 	const query = event.path.replace('/favicon', '')
-	const path = 'https://main-green-spider.b-cdn.net/' + query
+	const path = 'https://main-green-spider.b-cdn.net/' + query + '/32'
 	let response, blob, buffer, base64
 
 	try {
 		response = await fetch(path)
 		blob = await response.blob()
 		buffer = await blob.arrayBuffer()
-		base64 = 'data:image/png;base64,' + btoa(String.fromCharCode(...new Uint8Array(buffer)))
+		base64 = 'data:image/png;base64,' + Buffer.from(String.fromCharCode(...new Uint8Array(buffer))).toString('base64')
 	} catch (err) {
 		return {
 			statusCode: err.statusCode || 500,
@@ -24,8 +24,8 @@ exports.handler = async (event, context) => {
 		statusCode: 200,
 		body: base64,
 		headers: {
-			'content-type': 'application/text',
 			'content-length': base64.length,
+			'content-type': 'application/text',
 			'access-control-allow-origin': '*',
 		},
 	}
